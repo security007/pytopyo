00382e6729eb293cc0e8561c3f41bbd6a428f70e
security007 <35357051+security007@users.noreply.github.com>
2019-03-09 17:17:23 +0700

    Add files via upload

---
+++ pytopyo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+print """
+=======PYTHON OBFUSCATE AND COMPILER TOOL============
+"""
+ioioiooioiooiooioiooo = "66726f6d206f7320696d706f72742073797374656d2061732069696f696f6969696f696f".decode('hex')
+ooioioioiooioooioioio = "66726f6d2073797320696d706f72742061726776206173206f696f696f696f696f69696f69".decode('hex')
+ilililililil = "200a696620286c656e286f696f696f696f696f69696f6929203d3d2032293a0a096c6c6c6c203d2022373037393734363836663665323032643466346632303264366432303730373935663633366636643730363936633635222e6465636f6465282768657827290a09787878203d2069696f696f6969696f696f286c6c6c6c2b2220222b6f696f696f696f696f69696f695b315d290a097072696e742022446f6e6521220a656c73653a0a097072696e742022557361676520222b6f696f696f696f696f69696f695b305d2b222066696c652e7079220a"
+ljljljljljl = ilililililil.decode('hex')
+eval(compile(ioioiooioiooiooioiooo,'<string>','exec'))
+eval(compile(ooioioioiooioooioioio,'<string>','exec'))
+eval(compile(ljljljljljl,'<string>','exec'))
+a = oioioioioiioi[1]
+b = a.replace(".py",".pyo")
+print "File saved as "+b
+print "Just run your script like this 'python "+b+"'"
